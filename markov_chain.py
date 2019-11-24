5c73ed3362fe86d0c57f27bca13d2a61c7dffc1d
carbonecar <carbonecar@gmail.com>
2019-11-24 13:28:35 -0300

    first commit

---
+++ markov_chain.py
@@ -0,0 +1,33 @@
+import numpy as np
+first_word=input('ingresa una palabra significativa relacionada con economía (vacio para tomar una palabra al azar):  ')
+gobierno=open("hazzlit.txt",encoding='UTF-8').read()
+
+corpus=gobierno.split()
+
+def make_pairs(corpus):
+    for i in range(len(corpus)-1):
+        yield(corpus[i],corpus[i+1])
+
+pairs=make_pairs(corpus)
+
+word_dict={}
+
+for word_1,word_2 in pairs: 
+    if word_1 in word_dict.keys():
+        word_dict[word_1].append(word_2)
+    else:
+        word_dict[word_1]=[word_2]
+
+if first_word == '':
+    first_word =np.random.choice(corpus)
+print(first_word);
+chain=[first_word]
+
+n_words=50
+
+for i in range(n_words):
+    chain.append(np.random.choice(word_dict[chain[-1]]))
+
+print(' '.join(chain))
+
+
